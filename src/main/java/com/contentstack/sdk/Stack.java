c0dc9bf04617668b94348584b1b41f6cf0b0de9e
mshaileshr@gmail.com <Yes@383840>
2019-08-26 15:29:12 +0530

    v1.4.1 # Removed support for Google Internet Connection check before call # Added support of whereIn and whereNotIn for $in_query and $nin_query support in Query class

--- src/main/java/com/contentstack/sdk/Stack.java
+++ src/main/java/com/contentstack/sdk/Stack.java
@@ -34,7 +34,7 @@ import java.util.*;
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
@@ -98,16 +98,6 @@ public class Stack {
 
         if(!config.environment.isEmpty()){
             setHeader("environment", config.environment);
-        }
-
-        if (!config.region.name().isEmpty()){
-            String region = config.region.name().toLowerCase();
-            if (!region.equalsIgnoreCase("us")){
-                if(URL.equalsIgnoreCase("cdn.contentstack.io")) {
-                    URL=URL.replace("io", "com");
-                    URL = region + "-" + URL;
-                }
-            }
         }
 
     }
