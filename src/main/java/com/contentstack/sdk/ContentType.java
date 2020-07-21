d1eaf7cb51a7f9e8a2de28503e02bac21212ec93
mshaileshr@gmail.com <Yes@383840>
2020-07-21 13:37:03 +0530

    ## 1.5.2:    ### Date: 21-July-2020    - [Bump Issue] : Bump Issue: log4j-core from 2.5 to 2.13.2    - [Query] : Query.locale() documentation    - [ CSHttpConnection ] : StandardCharsets.UTF_8 Support Added

--- src/main/java/com/contentstack/sdk/ContentType.java
+++ src/main/java/com/contentstack/sdk/ContentType.java
@@ -11,27 +11,9 @@ import java.util.Map;
 
 /**
  *
- * MIT License
- *
- * Copyright (c) 2012 - 2019 Contentstack
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * A Content Type is the structure or blueprint of a page or a section that your web or mobile
+ * property will display. It lets you define the overall schema of this blueprint by adding fields
+ * and setting its properties.
  */
 
 public class ContentType {
