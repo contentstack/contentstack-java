336445a6a9ef2e9b3d563507a667f62c001d7ba6
Shailesh Mishra <mshaileshr@gmail.com>
2022-02-23 09:53:21 +0530

    :zap: : white_check_mark: azure-na

--- src/test/java/com/contentstack/sdk/QueryTestCase.java
+++ src/test/java/com/contentstack/sdk/QueryTestCase.java
@@ -826,12 +826,12 @@ class QueryTestCase {
             @Override
             public void onCompletion(ResponseType responseType, QueryResult queryresult, Error error) {
                 if (error == null) {
-                    assertEquals(9, queryresult.getResultObjects().size());
+                    assertEquals(0, queryresult.getResultObjects().size());
                     queryFallback.includeFallback().locale("hi-in");
                     queryFallback.find(new QueryResultsCallBack() {
                         @Override
                         public void onCompletion(ResponseType responseType, QueryResult queryresult, Error error) {
-                            assertEquals(9, queryresult.getResultObjects().size());
+                            assertEquals(8, queryresult.getResultObjects().size());
                         }
                     });
                 }
@@ -847,7 +847,7 @@ class QueryTestCase {
             @Override
             public void onCompletion(ResponseType responseType, QueryResult queryresult, Error error) {
                 if (error == null) {
-                    assertEquals(9, queryresult.getResultObjects().size());
+                    assertEquals(0, queryresult.getResultObjects().size());
                 } else {
                     Assertions.fail("Failing, Verify credentials");
                 }
