369b2f1434f68f35043e7827237849f5d0962c3d
Shailesh Mishra <mshaileshr@gmail.com>
2023-08-10 13:57:52 +0530

    GitHub Package Release

--- src/test/java/com/contentstack/sdk/Credentials.java
+++ src/test/java/com/contentstack/sdk/Credentials.java
@@ -1,11 +1,14 @@
 package com.contentstack.sdk;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvException;
 import lombok.var;
 
+import java.io.File;
 import java.rmi.AccessException;
 
 public class Credentials {
-    //static Dotenv env = getEnv();
+    static Dotenv env = getEnv();
 
     private static String envChecker() {
         String githubActions = System.getenv("GITHUB_ACTIONS");
@@ -20,45 +23,38 @@ public class Credentials {
         }
     }
 
-//    public static Dotenv getEnv() {
-//        String currentDirectory = System.getProperty("user.dir");
-//        File envFile = new File(currentDirectory, "env");
-//        env = Dotenv.configure()
-//                .directory("src/test/resources")
-//                .filename("env") // instead of '.env', use 'env'
-//                .load();
-//        try {
-//            env = Dotenv.load();
-//        } catch (DotenvException ex) {
-//            System.out.println("Could not load from local .env");
-////            File envFile = new File(currentDirectory, ".env");
-////            try {
-////                // Create .env file in the current directory
-////                envFile.createNewFile();
-////            } catch (IOException e) {
-////                System.err.println("An error occurred while creating .env file.");
-////                e.printStackTrace();
-////            }
-//        }
-//        return env;
-//    }
+    public static Dotenv getEnv() {
+        String currentDirectory = System.getProperty("user.dir");
+        File envFile = new File(currentDirectory, "env");
+        env = Dotenv.configure()
+                .directory("src/test/resources")
+                .filename("env") // instead of '.env', use 'env'
+                .load();
+        try {
+            env = Dotenv.load();
+        } catch (DotenvException ex) {
+            System.out.println("Could not load from local .env");
+//            File envFile = new File(currentDirectory, ".env");
+//            try {
+//                // Create .env file in the current directory
+//                envFile.createNewFile();
+//            } catch (IOException e) {
+//                System.err.println("An error occurred while creating .env file.");
+//                e.printStackTrace();
+//            }
+        }
+        return env;
+    }
 
 
-//    public final static String pwd = (env.get("PWD") != null) ? env.get("PWD") : "contentstack-java";
-//    public final static String HOST = (env.get("HOST") != null) ? env.get("HOST") : "cdn.contentstack.io";
-//    public final static String API_KEY = (env.get("API_KEY") != null) ? env.get("API_KEY") : "blt12c8ad610ff4ddc2";
-//    public final static String DELIVERY_TOKEN = (env.get("DELIVERY_TOKEN") != null) ? env.get("DELIVERY_TOKEN") : "cs9c1afdffa298f8708e3459e4";
-//    public final static String ENVIRONMENT = (env.get("ENVIRONMENT") != null) ? env.get("ENVIRONMENT") : "env1";
-//    public final static String CONTENT_TYPE = (env.get("contentType") != null) ? env.get("contentType") : "product";
-//    public final static String ENTRY_UID = (env.get("assetUid") != null) ? env.get("assetUid") : "blt884786476373";
+    public final static String pwd = (env.get("PWD") != null) ? env.get("PWD") : "contentstack-java";
+    public final static String HOST = (env.get("HOST") != null) ? env.get("HOST") : "cdn.contentstack.io";
+    public final static String API_KEY = (env.get("API_KEY") != null) ? env.get("API_KEY") : "";
+    public final static String DELIVERY_TOKEN = (env.get("DELIVERY_TOKEN") != null) ? env.get("DELIVERY_TOKEN") : "";
+    public final static String ENVIRONMENT = (env.get("ENVIRONMENT") != null) ? env.get("ENVIRONMENT") : "env1";
+    public final static String CONTENT_TYPE = (env.get("contentType") != null) ? env.get("contentType") : "product";
+    public final static String ENTRY_UID = (env.get("assetUid") != null) ? env.get("assetUid") : "";
 
-
-    public final static String HOST = "cdn.contentstack.io";
-    public final static String API_KEY = "blt12c8ad610ff4ddc2";
-    public final static String DELIVERY_TOKEN = "cs9c1afdffa298f8708e3459e4";
-    public final static String ENVIRONMENT = "env1";
-    public final static String CONTENT_TYPE = "product";
-    public final static String ENTRY_UID = "blt884786476373";
 
     private static volatile Stack stack;
 
