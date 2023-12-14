748c92c61e5eeaf3a3c7186de7745fb2b2306890
Shailesh Mishra <mshaileshr@gmail.com>
2023-12-15 00:48:32 +0530

    v1.12.3 - Taxonomy query support - Early Access Feature Support

--- src/main/java/com/contentstack/sdk/APIService.java
+++ src/main/java/com/contentstack/sdk/APIService.java
@@ -4,7 +4,7 @@ import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.HeaderMap;
-import retrofit2.http.QueryMap;
+import retrofit2.http.Query;
 import retrofit2.http.Url;
 
 import java.util.LinkedHashMap;
@@ -23,5 +23,5 @@ public interface APIService {
     @GET("v3/taxonomies/entries")
     Call<ResponseBody> getTaxonomy(
             @HeaderMap Map<String, Object> headers,
-            @QueryMap Map<String, Object> query);
+            @Query("query") String query);
 }
