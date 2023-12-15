9c725ac78008114f1976ac752e5c9cc0c055f978
Shailesh Mishra <mshaileshr@gmail.com>
2023-12-15 20:32:15 +0530

    v1.12.3 - Taxonomy query support - Early Access Feature Support

--- src/main/java/com/contentstack/sdk/Config.java
+++ src/main/java/com/contentstack/sdk/Config.java
@@ -1,7 +1,5 @@
 package com.contentstack.sdk;
 
-import lombok.Getter;
-import lombok.Setter;
 import okhttp3.ConnectionPool;
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONObject;
@@ -19,7 +17,6 @@ public class Config {
     protected String livePreviewHash = null;
     protected String livePreviewContentType = null;
     protected String livePreviewEntryUid = null;
-    @Getter
     protected String host = "cdn.contentstack.io";
     protected String version = "v3";
     protected String scheme = "https://";
@@ -29,21 +26,48 @@ public class Config {
     protected JSONObject livePreviewEntry = null;
     protected ContentstackRegion region = ContentstackRegion.US;
     protected String managementToken;
-    @Setter
-    @Getter
     protected String branch;
-    @Setter
     protected Proxy proxy = null;
+    protected String[] earlyAccess = null;
     protected ConnectionPool connectionPool = new ConnectionPool();
 
     protected List<ContentstackPlugin> plugins = null;
 
+
+    public String[] getEarlyAccess() {
+        return this.earlyAccess;
+    }
+
+    public Config setEarlyAccess(String[] earlyAccess) {
+        this.earlyAccess = earlyAccess;
+        return this;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
     /**
-     * -- GETTER --
-     * The configuration for the contentstack that contains support for
+     * Proxy can be set like below.
+     *
+     * @param proxy Proxy setting, typically a type (http, socks) and a socket address. A Proxy is an immutable object
+     *              <br>
+     *              <br>
+     *              <b>Example:</b><br>
+     *              <br>
+     *              <code>
+     *              java.net.Proxy proxy = new Proxy(Proxy.Type.HTTP,  new InetSocketAddress("proxyHost", "proxyPort"));
+     *              java.net.Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("sl.theproxyvpn.io", 80)); Config
+     *              config = new Config(); config.setProxy(proxy);
+     *              </code>
      */
-    @Getter
-    protected String[] earlyAccess;
+    public void setProxy(Proxy proxy) {
+        this.proxy = proxy;
+    }
 
     /**
      * Returns the Proxy instance
@@ -99,6 +123,15 @@ public class Config {
 
     public void setPlugins(List<ContentstackPlugin> plugins) {
         this.plugins = plugins;
+    }
+
+    /**
+     * Gets host.
+     *
+     * @return the host
+     */
+    public String getHost() {
+        return host;
     }
 
     /**
@@ -164,25 +197,6 @@ public class Config {
      */
     public enum ContentstackRegion {
         US, EU, AZURE_NA, AZURE_EU
-    }
-
-
-    /**
-     * The configuration for the contentstack that contains support for Early Access Feature
-     *
-     * @param earlyAccessFeatures The list of Early Access Features
-     *                            {@code
-     *                            Config config = new Config();
-     *                            String[] earlyAccess = {"Taxonomy", "Teams", "Terms", "LivePreview"};
-     *                            config.earlyAccess(earlyAccess);
-     *                            Stack stack = Contentstack.stack(API_KEY, DELIVERY_TOKEN, ENV, config);
-     *                            <p>
-     *                            }
-     * @return Config
-     */
-    public Config earlyAccess(@NotNull String[] earlyAccessFeatures) {
-        this.earlyAccess = earlyAccessFeatures;
-        return this;
     }
 
 }
