332e93374829a434ed63df103c589f7938f074be
Shailesh Mishra <mshaileshr@gmail.com>
2023-12-15 14:38:57 +0530

    v1.12.3 - Taxonomy query support - Early Access Feature Support

--- src/main/java/com/contentstack/sdk/TaxonomyCallback.java
+++ src/main/java/com/contentstack/sdk/TaxonomyCallback.java
@@ -5,6 +5,11 @@ import org.json.JSONObject;
 
 public interface TaxonomyCallback {
 
+    /**
+     * This method is called wen API response gets received
+     * @param response the response of type JSON
+     * @param error the error of type @{@link Error}
+     */
     void onResponse(JSONObject response, Error error);
 
 }
