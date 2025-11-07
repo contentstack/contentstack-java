336b07fb25d1bf4d31c5039dcd3e95cd6977aa37
harshitha.d <harshitha.d@contentstack.com>
2025-11-07 12:04:41 +0530

    Add integration tests for ContentstackPlugin, implementing two sample plugins and validating their request/response handling in the Contentstack environment.

--- src/test/java/com/contentstack/sdk/ContentstackPluginIT.java
+++ src/test/java/com/contentstack/sdk/ContentstackPluginIT.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class TestContentstackPluginIT {
+class ContentstackPluginIT {
 
     final Stack stack = Credentials.getStack();
 
